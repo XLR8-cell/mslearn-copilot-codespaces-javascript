2a5688ca9340ab506cd00d12b9ba144a083fb105
Gilbert Ondonga Nyamberi <hunchovault@gmail.com>
2024-06-19 16:41:40 +0000

    Copilot in Javascript

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Gilbert Ondonga",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "gilbertnyamberi1@gmail.com",
+  gitHub: "XLR8-cell",
+  instagram: "Gilbert",
+  linkedIn: "Gilbert Ondonga",
+  medium: "Gilbert Ondonga",
+  twitter: "Bucky_Burns_",
+  youTube: "Codelicious",
 };
 
 const primaryColor = "#4E567E";
